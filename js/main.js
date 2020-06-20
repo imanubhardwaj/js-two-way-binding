2d057a3869ba0c17ad4d2465ae2dd9c72ac8c232
Manu Bhardwaj <manu.bhardwaj@instasolv.com>
2020-06-20 20:52:11 +0530

    [update] add setter getter approach

--- js/main.js
+++ js/main.js
@@ -5,11 +5,29 @@ function setupBinding() {
     for (let el of elements) {
         if (el.type) {
             const bindKeyName = el.getAttribute("bind");
+            createGetterSetter(bindData, bindKeyName);
             el.onkeyup = function () {
                 bindData[bindKeyName] = this.value;
             };
         }
     }
+}
+
+function createGetterSetter(dataObj, key) {
+    Object.defineProperty(dataObj, key, {
+        get: function () {
+            return bindData[key];
+        },
+        set: function (value) {
+            for (let el of elements) {
+                const bindKeyName = el.getAttribute("bind");
+                if (bindKeyName === key) {
+                    el.innerText = value;
+                }
+            }
+        },
+        enumerable: true // So that it doesn't appear in Object.keys
+    });
 }
 
 function printEmailToConsole() {
